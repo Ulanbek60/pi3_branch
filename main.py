8d3dcd58c7bddeffce384c9ba695cc49829c3ddc
Ulanbek60 <uislambekuulu@gmail.com>
2025-10-23 08:27:03 +0600

    created test.py and modified main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print('Hello World from main.py')
 print('Hello World from main.py')
+
+
+
+for i in range(1, 11):
+    print(i)
---
+++ test.py
@@ -0,0 +1,4 @@
+input('write something')
+for i in range(1, 11):
+    print(i)
+input('write something')
